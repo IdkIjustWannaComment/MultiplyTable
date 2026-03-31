73dc700ef881376261068b07b453b2e5767bdc63
Nikhil Govender <nikhil.govender01@gmail.com>
2026-03-31 09:41:25 +0200

    Moved data from main activity to table activity

--- app/src/main/java/com/example/multiplicationtable/MainActivity.kt
+++ app/src/main/java/com/example/multiplicationtable/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.multiplicationtable
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,10 +16,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //added variable to hold multiplication button
         val btnMultiply = findViewById<Button>(R.id.btnMultiply)
+        //Added variable that holds the user's input
+        val numInput = findViewById<EditText>(R.id.edtNum)
         //a listener listens for a button that's been clicked
         btnMultiply.setOnClickListener {
-            
+
             val intent = Intent(this, MainActivity2::class.java)
+
+            intent.putExtra("tableNumber", numInput.text.toString())
             startActivity(intent)
         }
 

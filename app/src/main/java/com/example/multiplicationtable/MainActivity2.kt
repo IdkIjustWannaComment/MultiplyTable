c02c9ee09c29840e2fdcb018a45ff1c233630a41
Nikhil Govender <nikhil.govender01@gmail.com>
2026-03-31 09:53:29 +0200

    Added variables in main activity 2

--- app/src/main/java/com/example/multiplicationtable/MainActivity2.kt
+++ app/src/main/java/com/example/multiplicationtable/MainActivity2.kt
@@ -1,6 +1,8 @@
 package com.example.multiplicationtable
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,14 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+
+        //recieving data from the main activity
+        val bundle: Bundle? = intent.extras
+
+        //val tableString: Bundle?.getString("tableNumber")
+       // val tableNumber = tableString?.toInt()
+        val multiplyTbl = findViewById<TextView>(R.id.txtMultiplyTextView)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
